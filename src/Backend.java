1febfddb4729fcf72a106e32319ae7c300ad9bbb
SF49ERS7 <51031575+SF49ERS7@users.noreply.github.com>
2022-01-21 19:41:57 -0800

    Added support for pretty numbers

--- src/Backend.java
+++ src/Backend.java
@@ -1,17 +1,13 @@
-import java.io.FileWriter;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
+import java.text.DecimalFormat;
 /**
- * Handles the parsing of inputted data, as well as the aforementioned outputting to a text file.
+ * Handles the parsing of inputted data.
  */
 public class Backend
 {
     /**
      * Stores the program's version number
      */
-    private static final String programVersion = "0.3.2-alpha";
+    private static final String programVersion = "0.4.0-alpha";
     /**
      * Getter for <code>programVersion</code>.
      * @return The program's version.
@@ -35,35 +31,15 @@ public class Backend
         return input.toLowerCase();
     }
     /**
-     * Sends the <code>output</code> parameter to a file on the disk.
-     * @param output The data to be sent to the disk.
-     */
-    public static void sendToTextFile(String output)
-    {
-        try {
-            Date date = new Date(); // This object contains the current date value
-            SimpleDateFormat formatter = new SimpleDateFormat("MM-dd-yyyy 'at' HH:mm:ss");
-
-            FileWriter outputFile = new FileWriter("output.txt");
-            outputFile.write("#Character Counter File Output, by SF49ERS7\n#Version " + getProgramVersion() + "\n#Generated on " + formatter.format(date) + "\n\n" + output);
-            outputFile.close();
-        } catch (IOException e) {
-            if (Config.getRunUI().equals("GUI"))
-                GUI.displayFileError();
-            else
-                CLI.displayFileError();
-        }
-    }
-    /**
      * Takes the <code>data</code> parameter and calculates how many of each character there are in it.
      * @param data The input generated at the beginning of the program.
      * @return The number of each characters found.
      */
-    public static int[] charCounter(String data)
+    public static long[] charCounter(String data)
     {
-        int[] numsOfChars = new int[29];
-        for (int num : numsOfChars)
-            numsOfChars[num] = 0;
+        long[] numsOfChars = new long[29];
+        for (long num : numsOfChars)
+            numsOfChars[(int) num] = 0;
 
         for (int i = 0; i < data.length(); i++)
             switch (data.charAt(i)) {
@@ -113,39 +89,39 @@ public class Backend
      * @param numsOfChars The array that tells how many of each character there are.
      * @return A concatenated <code>String</code> with everything formatted.
      */
-    public static String outputMessage(int[] numsOfChars)
+    public static String outputMessage(long[] numsOfChars)
     {
         if (Config.getEnabledSettings()[2])
             return "Number of characters:\n" +
-                    "A: " + numsOfChars[0] + "\n" +
-                    "B: " + numsOfChars[1] + "\n" +
-                    "C: " + numsOfChars[2] + "\n" +
-                    "D: " + numsOfChars[3] + "\n" +
-                    "E: " + numsOfChars[4] + "\n" +
-                    "F: " + numsOfChars[5] + "\n" +
-                    "G: " + numsOfChars[6] + "\n" +
-                    "H: " + numsOfChars[7] + "\n" +
-                    "I: " + numsOfChars[8] + "\n" +
-                    "J: " + numsOfChars[9] + "\n" +
-                    "K: " + numsOfChars[10] + "\n" +
-                    "L: " + numsOfChars[11] + "\n" +
-                    "M: " + numsOfChars[12] + "\n" +
-                    "N: " + numsOfChars[13] + "\n" +
-                    "O: " + numsOfChars[14] + "\n" +
-                    "P: " + numsOfChars[15] + "\n" +
-                    "Q: " + numsOfChars[16] + "\n" +
-                    "R: " + numsOfChars[17] + "\n" +
-                    "S: " + numsOfChars[18] + "\n" +
-                    "T: " + numsOfChars[19] + "\n" +
-                    "U: " + numsOfChars[20] + "\n" +
-                    "V: " + numsOfChars[21] + "\n" +
-                    "W: " + numsOfChars[22] + "\n" +
-                    "X: " + numsOfChars[23] + "\n" +
-                    "Y: " + numsOfChars[24] + "\n" +
-                    "Z: " + numsOfChars[25] + "\n" +
-                    "Numbers: " + numsOfChars[26] + "\n" +
-                    "Spaces: " + numsOfChars[27] + "\n" +
-                    "Other Characters: " + numsOfChars[28];
+                    "A: " + format(numsOfChars[0]) + "\n" +
+                    "B: " + format(numsOfChars[1]) + "\n" +
+                    "C: " + format(numsOfChars[2]) + "\n" +
+                    "D: " + format(numsOfChars[3]) + "\n" +
+                    "E: " + format(numsOfChars[4]) + "\n" +
+                    "F: " + format(numsOfChars[5]) + "\n" +
+                    "G: " + format(numsOfChars[6]) + "\n" +
+                    "H: " + format(numsOfChars[7]) + "\n" +
+                    "I: " + format(numsOfChars[8]) + "\n" +
+                    "J: " + format(numsOfChars[9]) + "\n" +
+                    "K: " + format(numsOfChars[10]) + "\n" +
+                    "L: " + format(numsOfChars[11]) + "\n" +
+                    "M: " + format(numsOfChars[12]) + "\n" +
+                    "N: " + format(numsOfChars[13]) + "\n" +
+                    "O: " + format(numsOfChars[14]) + "\n" +
+                    "P: " + format(numsOfChars[15]) + "\n" +
+                    "Q: " + format(numsOfChars[16]) + "\n" +
+                    "R: " + format(numsOfChars[17]) + "\n" +
+                    "S: " + format(numsOfChars[18]) + "\n" +
+                    "T: " + format(numsOfChars[19]) + "\n" +
+                    "U: " + format(numsOfChars[20]) + "\n" +
+                    "V: " + format(numsOfChars[21]) + "\n" +
+                    "W: " + format(numsOfChars[22]) + "\n" +
+                    "X: " + format(numsOfChars[23]) + "\n" +
+                    "Y: " + format(numsOfChars[24]) + "\n" +
+                    "Z: " + format(numsOfChars[25]) + "\n" +
+                    "Numbers: " + format(numsOfChars[26]) + "\n" +
+                    "Spaces: " + format(numsOfChars[27]) + "\n" +
+                    "Other Characters: " + format(numsOfChars[28]);
         else
             return "Number of characters:\n" +
                 filterValues("A: ", numsOfChars[0], false) +
@@ -183,15 +159,25 @@ public class Backend
      * @param prefix What character is being counted.
      * @param input The number of characters, pre-counted.
      * @param isAtTheEnd Determines if the value should have a carriage return or not.
-     * @return The filtered value.
+     * @return The filtered and formatted value.
      */
-    public static String filterValues(String prefix, int input, boolean isAtTheEnd)
+    public static String filterValues(String prefix, long input, boolean isAtTheEnd)
     {
         if (input == 0 && !Config.getEnabledSettings()[2])
             return "";
         else if (!isAtTheEnd)
-            return prefix + input + "\n";
+            return prefix + format(input) + "\n";
         else
-            return prefix + input;
+            return prefix + format(input);
+    }
+    /**
+     * Formats an inputted number.
+     * @param input The number to format.
+     * @return The formatted number.
+     */
+    public static String format(long input)
+    {
+        DecimalFormat formatter = new DecimalFormat("#,###");
+        return formatter.format(input);
     }
 }
