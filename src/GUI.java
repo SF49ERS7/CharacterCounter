036b4704714efbe83a3cbcb832e8a8a7738c4fed
SF49ERS7 <51031575+SF49ERS7@users.noreply.github.com>
2022-03-24 14:47:15 -0700

    Add settings UI

--- src/GUI.java
+++ src/GUI.java
@@ -3,12 +3,17 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 import static java.util.Objects.requireNonNullElse;
+import static javax.swing.JOptionPane.ERROR_MESSAGE;
+import static javax.swing.JOptionPane.INFORMATION_MESSAGE;
+import static javax.swing.JOptionPane.QUESTION_MESSAGE;
+import static javax.swing.JOptionPane.YES_NO_CANCEL_OPTION;
 import static javax.swing.JOptionPane.showInputDialog;
 import static javax.swing.JOptionPane.showMessageDialog;
- /**
+/**
  *  Contains all the <code>JOptionPane</code> calls in this program, making modularization easy.
  */
 public class GUI
@@ -19,7 +24,7 @@ public class GUI
      */
     public static String displayHIDInput()
     {
-        String input = showInputDialog(null, "Enter the input to count\nAlternatively, type ':goback' to go back", "Input", JOptionPane.QUESTION_MESSAGE);
+        String input = showInputDialog(null, "Enter the input to count\nAlternatively, type ':goback' to go back", "Input", QUESTION_MESSAGE);
         return requireNonNullElse(input, ":goback");
     }
     /**
@@ -28,21 +33,21 @@ public class GUI
      */
     public static void displayOutput(long[] numsOfChars)
     {
-        showMessageDialog(null, Backend.outputMessage(numsOfChars), "Result", JOptionPane.INFORMATION_MESSAGE);
+        showMessageDialog(null, Backend.outputMessage(numsOfChars), "Result", INFORMATION_MESSAGE);
     }
     /**
      * Displays when the user fails to provide input.
      */
     public static void noInputError()
     {
-        showMessageDialog(null, "Error: You gave no input\nPlease give an input and try again", "Error", JOptionPane.ERROR_MESSAGE);
+        showMessageDialog(null, "Error: You gave no input\nPlease give an input and try again", "Error", ERROR_MESSAGE);
     }
     /**
      * Displays when the user provides a file that does not contain text.
      */
     public static void invalidInputError()
     {
-        showMessageDialog(null, "Error: File either contains no data, or is unreadable.", "Error", JOptionPane.ERROR_MESSAGE);
+        showMessageDialog(null, "Error: File either contains no data, or is unreadable.", "Error", ERROR_MESSAGE);
     }
     /**
      * Tests if the inputted data is empty.
@@ -56,6 +61,26 @@ public class GUI
             return true;
         }
         return false;
+    }
+    /**
+     * Saves the counted output to a file
+     * @param numsOfChars The array of counted characters.
+     */
+    public static void saveOutputToFile(long[] numsOfChars)
+    {
+        JFrame frame = new JFrame("Input");
+        JFileChooser chooser = new JFileChooser();
+        chooser.setDialogTitle("Select where you wish to save the file output");
+        frame.setSize(1,1);
+        frame.setVisible(true);
+        frame.setVisible(false);
+        int returnVal = chooser.showSaveDialog(frame);
+        if (returnVal == JFileChooser.APPROVE_OPTION)
+            try {
+                Backend.writeOutputToFile(numsOfChars, chooser.getSelectedFile().getAbsolutePath());
+            } catch (IOException e) {
+                showMessageDialog(null, "Error: Disk is write-protected");
+            }
     }
     /**
      * Displays a <code>JFileChooser</code> prompt, asking for a file.
@@ -97,7 +122,7 @@ public class GUI
      */
     public static void noFileError()
     {
-        showMessageDialog(null, "Error: The file does not exist, or is invalid.", "Error", JOptionPane.ERROR_MESSAGE);
+        showMessageDialog(null, "Error: The file does not exist, or is invalid.", "Error", ERROR_MESSAGE);
     }
     /**
      * Runs the initial GUI.
@@ -106,11 +131,21 @@ public class GUI
     {
         for (long i = 0L; i < Long.MAX_VALUE; i++)
         {
-            Object[] options = new Object[]{"From inputted text", "From a file", "Cancel"};
-            int n = JOptionPane.showOptionDialog(null, "Welcome to Character Counter, version " + Backend.getProgramVersion() + "\nHow would you like to count?", "Input", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+            Object[] options;
+            if (Config.isShowSettingsButton())
+                options = new Object[]{"From inputted text", "From a file","Settings", "Cancel"};
+            else
+                options = new Object[]{"From inputted text", "From a file", "Cancel"};
+            int n = JOptionPane.showOptionDialog(null, "Welcome to Character Counter, version " + Backend.getProgramVersion() + "\nHow would you like to count?", "Input", YES_NO_CANCEL_OPTION, QUESTION_MESSAGE, null, options, options[0]);
             switch (n) {
                 case 0 -> hid();
                 case 1 -> fileCounter();
+                case 2 -> {
+                    if (Config.isShowSettingsButton())
+                        settingsUI();
+                    else
+                        System.exit(0);
+                }
                 default -> System.exit(0);
             }
         }
@@ -138,8 +173,11 @@ public class GUI
             long[] numsOfChars = Backend.charCounter(parsedData);
 
             displayOutput(numsOfChars);
+            if (Config.isSendOutputToFile())
+                saveOutputToFile(numsOfChars);
         }
     }
+
     /**
      * Runs the GUI that counts from a file on disk.
      */
@@ -191,8 +229,44 @@ public class GUI
             long[] numsOfChars = Backend.charCounter(input);
 
             displayOutput(numsOfChars);
+            if (Config.isSendOutputToFile())
+                saveOutputToFile(numsOfChars);
 
             resetFileInputPath();
+        }
+    }
+    /**
+     * Runs the settings UI.
+     */
+    public static void settingsUI()
+    {
+        for (long i = 0; i < Long.MAX_VALUE; i++)
+        {
+            Object[] validSettings = new Object[]{"Show empty values", "Send output to a file", "Show total characters", "Count numbers individually"};
+            String settingToChange = String.valueOf(showInputDialog(null, "Select the setting you wish to change, or click Cancel if you are done.", "Input", QUESTION_MESSAGE, null, validSettings, validSettings[0]));
+            boolean whatSettingWasSetTo;
+            switch (settingToChange) {
+                case "Show empty values" -> {
+                    Config.setShowEmptyVals(!Config.isShowEmptyVals());
+                    whatSettingWasSetTo = Config.isShowEmptyVals();
+                }
+                case "Send output to a file" -> {
+                    Config.setSendOutputToFile(!Config.isSendOutputToFile());
+                    whatSettingWasSetTo = Config.isSendOutputToFile();
+                }
+                case "Show total characters" -> {
+                    Config.setShowTotal(!Config.isShowTotal());
+                    whatSettingWasSetTo = Config.isShowTotal();
+                }
+                case "Count numbers individually" -> {
+                    Config.setShowNumsIndependently(!Config.isShowNumsIndependently());
+                    whatSettingWasSetTo = Config.isShowNumsIndependently();
+                }
+                default -> {
+                    return;
+                }
+            }
+            showMessageDialog(null, settingToChange + " is now set to " + whatSettingWasSetTo + ".");
         }
     }
 }
