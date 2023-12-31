4e3fc13975a91483bcd15c75b5a2e0fd1a066bd3
SF49ERS7 <51031575+SF49ERS7@users.noreply.github.com>
2023-12-30 23:06:32 -0800

    Add Javadoc

--- src/RightClickListener.java
+++ src/RightClickListener.java
@@ -1,16 +1,31 @@
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+/**
+ * Handles right-click in the input counter.
+ */
 public class RightClickListener extends MouseAdapter
 {
+    /**
+     * Runs when the right click button is clicked
+     * @param e The event to be processed
+     */
     public void mousePressed(MouseEvent e) {
         possiblyShowRightClick(e);
     }
 
+    /**
+     * Runs when the right click button is released.
+     * @param e The event to be processed.
+     */
     public void mouseReleased(MouseEvent e) {
         possiblyShowRightClick(e);
     }
 
+    /**
+     * Calls the right click menu.
+     * @param e The event to be processed.
+     */
     private void possiblyShowRightClick(MouseEvent e) {
         if (e.isPopupTrigger()) {
             GUI.popupMenu.show(e.getComponent(),
